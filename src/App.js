37d657651f4f7cdee026e586e9690b06fdd7216f
Mahadi001 <sadat.himel@gmail.com>
2021-10-28 18:56:07 +0600

    update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id)=>{
+    const newTours = tours.filter((tour)=> tour.id !== id );
+    setTours(newTours);
+  }
+
   const fetchTours = async () => {
     setLoading(true);
-
     try {
       const response = await fetch(url);
       const tours = await response.json();
@@ -33,9 +37,20 @@ function App() {
       </main>
     );
   }
+
+  if ( tours.length === 0){
+    return(
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={()=>fetchTours()}>refresh</button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
